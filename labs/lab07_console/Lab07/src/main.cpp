50c4ae6f3d281002f0c234d4ee9f27a5fc36d7d1
user <user@DESKTOP-RG73B0N>
2026-02-11 11:44:38 +0700

    Sdelano

--- labs/lab07_console/Lab07/src/main.cpp
+++ labs/lab07_console/Lab07/src/main.cpp
@@ -14,14 +14,36 @@ int main() {
         return 1;
     }
 
-    const int N = 10;
+    const int N_POINTS = 10;
     const double x_end = 4.0;
-    double step = (x_end - x0) / (N - 1);
+    double step = (x_end - x0) / (N_POINTS - 1);
 
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N_POINTS; ++i) {
         double x = x0 + i * step;
         std::cout << "x = " << x << ", f(x) = " << f(x) << "\n";
     }
+
+    long long max_value;
+    std::cin >> max_value;
+
+    if (max_value < 0) {
+        std::cout << "0 0\n";
+        return 0;
+    }
+
+    long long a = 0, b = 1;
+    long long S = 0;
+    int N = 0;
+
+    while (S + a <= max_value) {
+        S += a;
+        N++;
+        long long temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    std::cout << N << " " << S << "\n";
 
     return 0;
 }
