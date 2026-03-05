ea136be983032fb12f9840ba99e3e88648c3adde
user <user@DESKTOP-RG73B0N>
2026-03-06 00:53:14 +0700

    pack unpack pack unpak lab10

--- labs/lab10_functions/Lab10/src/main.cpp
+++ labs/lab10_functions/Lab10/src/main.cpp
@@ -1,13 +1,58 @@
 #include <iostream>
+using namespace std;
 
-// Lab 10
-// TODO: реализуйте решение по заданию в labs/lab10_functions/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
+const int Max_znach = 17;
+const int Kolvo_znach = 18;
+
+void pack(const int source[], int n, int packed[]) {
+    for (int i = 0; i < Kolvo_znach; i++) packed[i] = 0;
+    for (int i = 0; i < n; i++) packed[source[i]]++;
+}
+
+void unpack(const int packed[], int restored[], int n) {
+    int index = 0;
+    for (int val = 0; val < Kolvo_znach; val++) {
+        for (int count = 0; count < packed[val]; count++) {
+            restored[index++] = val;
+        }
+    }
+}
+
+
 int main() {
-    // TODO
+    const int N = 1000000;
+    int* original = new int[N];
+
+    int pos = 0;
+    for (int val = 0; val <= Max_znach; val++) {
+        int count = N / Kolvo_znach;
+
+        if (val < N % Kolvo_znach) count++;
+        for (int j = 0; j < count; j++) original[pos++] = val;
+    }
+
+
+
+    cout << "Ishod. massiv: " << N << " elementov, "
+        << N * sizeof(int) << " bt" << endl;
+
+    int packed[Kolvo_znach] = { 0 };
+    pack(original, N, packed);
+
+    cout << "Upakovano: " << Kolvo_znach << " elementov, "
+        << Kolvo_znach * sizeof(int) << " bt" << endl;
+
+
+
+    int* restored = new int[N];
+    unpack(packed, restored, N);
+
+    cout << "Raspakovano: " << N << " elementov, "
+        << N * sizeof(int) << " bt" << endl;
+
+
+    delete[] original;
+    delete[] restored;
+
     return 0;
 }
